5ff774adf7e262cf74deb164433d4ea2d5c16303
Matthieu Napoli <matthieu@mnapoli.fr>
2017-01-28 23:50:02 +0100

    Test constructor arguments

--- tests/CreateDefinitionTest.php
+++ tests/CreateDefinitionTest.php
@@ -14,8 +14,7 @@ class CreateDefinitionTest extends \PHPUnit_Framework_TestCase
     public function test_create_with_class_name_provided()
     {
         $container = new ContainerBuilder;
-        $loader = new PhpConfigLoader($container);
-        $loader->load([
+        (new PhpConfigLoader($container))->load([
             'foo' => create('stdClass'),
         ]);
         self::assertInstanceOf('stdClass', $container->get('foo'));
@@ -24,11 +23,28 @@ class CreateDefinitionTest extends \PHPUnit_Framework_TestCase
     public function test_create_with_class_name_as_array_key()
     {
         $container = new ContainerBuilder;
-        $loader = new PhpConfigLoader($container);
-        $loader->load([
+        (new PhpConfigLoader($container))->load([
             'stdClass' => create(),
         ]);
         self::assertInstanceOf('stdClass', $container->get('stdClass'));
+    }
+
+    public function test_create_with_constructor_arguments()
+    {
+        $fixture = new class() {
+            public function __construct()
+            {
+                $this->arguments = func_get_args();
+            }
+        };
+        $className = get_class($fixture);
+
+        $container = new ContainerBuilder;
+        (new PhpConfigLoader($container))->load([
+            'foo' => create($className)
+                ->constructor('abc', 'def'),
+        ]);
+        self::assertEquals(['abc', 'def'], $container->get('foo')->arguments);
     }
 
     public function test_same_method_can_be_called_multiple_times()
@@ -43,8 +59,7 @@ class CreateDefinitionTest extends \PHPUnit_Framework_TestCase
         $className = get_class($fixture);
 
         $container = new ContainerBuilder;
-        $loader = new PhpConfigLoader($container);
-        $loader->load([
+        (new PhpConfigLoader($container))->load([
             $className => create()
                 ->method('increment')
                 ->method('increment'),
